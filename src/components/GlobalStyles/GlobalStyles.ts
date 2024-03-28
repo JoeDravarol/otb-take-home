f1eac81a9c6ea37211b807849c8d2dca23df0fa8
Joe Dravarol <joedravarol@gmail.com>
2024-03-28 14:49:17 +0000

    chore: setup design tokens

---
+++ src/components/GlobalStyles/GlobalStyles.ts
@@ -0,0 +1,62 @@
+import { createGlobalStyle } from 'styled-components';
+import '@fontsource/metropolis/400.css';
+import '@fontsource/metropolis/500.css';
+import '@fontsource/metropolis/600.css';
+
+import { COLORS, FAMILES, WEIGHTS } from '../../constant/design-token';
+
+const GlobalStyles = createGlobalStyle`
+/*
+  Josh's Custom CSS Reset
+  https://www.joshwcomeau.com/css/custom-css-reset/
+*/
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+* {
+  margin: 0;
+}
+body {
+  line-height: 1.5;
+  -webkit-font-smoothing: antialiased;
+}
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
+}
+input, button, textarea, select {
+  font: inherit;
+}
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
+}
+#root, #__next {
+  isolation: isolate;
+}
+
+/* DESIGN TOKENS */
+html {
+  --color-white: ${COLORS.white};
+  --color-black: ${COLORS.black};
+  --color-gray-400: ${COLORS.gray[400]};
+  --color-gray-500: ${COLORS.gray[500]};
+  --color-primary: ${COLORS.primary};
+  --color-accent-light: ${COLORS.accent.light};
+
+  --font-family-sans-serif: ${FAMILES.sansSerif};
+
+  --font-weight-normal: ${WEIGHTS.normal};
+  --font-weight-medium: ${WEIGHTS.medium};
+  --font-weight-bold: ${WEIGHTS.bold};
+}
+
+/* GLOBAL STYLES */
+*,
+*:before,
+*:after {
+  line-height: 1.5;
+  font-family: var(--font-family-sans-serif);
+}
+`;
+
+export default GlobalStyles;
---
+++ src/constant/design-token.ts
@@ -0,0 +1,22 @@
+export const COLORS = {
+  white: '#FFF',
+  black: '#333',
+  gray: {
+    400: '#D4D4D4',
+  },
+  primary: '#17317F', // Blue - Brand color
+  accent: {
+    light: '#FEDC07', // Yellow
+  },
+};
+
+export const FAMILES = {
+  sansSerif:
+    '"Metropolis", Helvetica, "Franklin Gothic Medium", "Franklin Gothic", "ITC Franklin Gothic", sans-serif',
+};
+
+export const WEIGHTS = {
+  normal: 400,
+  medium: 500,
+  bold: 600,
+};
